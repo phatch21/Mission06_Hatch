5b1cfed037fea22a250ee9e4a1b7a97201e27fa2
phatch21 <phatch21@byu.edu>
2025-02-10 13:10:44 -0700

    Add project files.

---
+++ Mission_6/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission_6.Models;
+
+namespace Mission_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnow()
+        {
+            return View();
+        }
+        public IActionResult Collection()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
